3a1574687654a8df3df19ca84658286b25d7170e
Eliezer Boeira <eliezerbuczak2016@gmail.com>
2024-01-08 16:58:46 -0300

    created

--- server/controller/machineController.js
+++ server/controller/machineController.js
@@ -8,12 +8,23 @@ export const searchMachine = async () => {
     })
 };
 export const saveMachine = async (event) => {
-    const request = await readBody(event);
-    const { ip, name, location } = request;
-    return await new Promise((resolve, reject) => {
-        db.run(`INSERT INTO machines (ip, name, location) VALUES (?, ?, ?)`, [ip, name, location], (err) => {
-            if (err) reject(err);
-            resolve({ id: this.lastID });
+    try {
+        const request = await readBody(event);
+        const { ip, name, location } = request;
+
+        return await new Promise((resolve, reject) => {
+            db.run(`INSERT INTO machines (ip, name, location) VALUES (?, ?, ?)`, [ip, name, location], function(err) {
+                if (err) {
+                    console.error('Erro ao inserir na base de dados:', err);
+                    reject(err);
+                } else {
+                    console.log('Inserção bem-sucedida!');
+                    resolve('ok');
+                }
+            });
         });
-    })
+    } catch (error) {
+        console.error('Erro ao processar a requisição:', error);
+        throw error;
+    }
 };
